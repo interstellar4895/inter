097a1ee9c2925763830ef9d8299bd0037e9560dc
interstellar4895 <99314828+interstellar4895@users.noreply.github.com>
2023-08-05 17:35:08 +0300

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xf40d9f517ffb5713b1f8c8ba1ce4ce261bf529d4";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
